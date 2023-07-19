0c94699ca738b55fdcbc6deb926832dc2dc70873
samvardhan777 <samvardhan777@gmail.com>
2023-07-19 10:46:21 +0530

    [VGS] add Exercise_1 solution

---
+++ data_visualization/Exercise_1.py
@@ -0,0 +1,60 @@
+import os
+
+import streamlit as st
+from snowflake.snowpark import Session
+from st_aggrid import AgGrid
+from st_aggrid.shared import JsCode
+from st_aggrid.grid_options_builder import GridOptionsBuilder
+from st_aggrid.shared import JsCode
+from snowflake.snowpark import Session
+import plotly.express as px
+import pandas as pd
+
+@st.cache_resource
+def create_session():
+    return Session.builder.configs({"account": os.environ["SNOWFLAKE_ACCOUNT"],
+                                    "database": os.environ["SNOWFLAKE_DATABASE"],
+                                    "user": os.environ["SNOWFLAKE_USER"],
+                                    "password": os.environ["SNOWFLAKE_PASSWORD"],
+                                    "role": "DEVELOPER",
+                                    "warehouse": "COMPUTE_WH",
+                                    "schema": "PSA",
+                                    "client_session_keep_alive": True}).create()
+
+
+def display_bar_chart(data_frame, x, y, title, xaxis_title, yaxis_title, width, height,color=
+                      'lightblue'):
+    fig = px.bar(data_frame, x=x, y=y, title=title,orientation='h')
+    fig.update_traces(marker_color=color)
+    fig.update_layout(width=width, height=height, margin=dict(l=50, r=50, b=50, t=50, pad=4))
+    fig.update_xaxes(title=xaxis_title)
+    fig.update_yaxes(title=yaxis_title)
+    st.plotly_chart(fig)
+
+
+if __name__ == '__main__':
+    st.set_page_config(
+        page_title="Data Dynamos",
+        page_icon="./assets/favicon.ico",
+        layout="wide",
+        initial_sidebar_state="expanded",
+    )
+
+    st.title("Total Emissions and Average Temperature 🚀 ")
+    session = create_session()
+
+    emission_temp = "EXERCISE_CO2_VS_TEMPERATURE.CARBON_EMISSIONS.AGGREGATE_COUNTRY_EMISSIONS_TEMPERATURES"
+    query = f"SELECT COUNTRY, YEAR, PERCAPITAEMISSIONS, SHAREOFGLOBALEMISSIONS, TOTALEMISSIONS, AVERAGETEMPERATURE FROM {emission_temp}"
+    emission_temperature = session._run_query(query)
+    emission_temperature = pd.DataFrame(emission_temperature, columns=['COUNTRY', 'YEAR', 'PERCAPITAEMISSIONS', 'SHAREOFGLOBALEMISSIONS', 'TOTALEMISSIONS', 'AVERAGETEMPERATURE'])
+
+
+    st.title("Total Emissions and Average Temperature for different Countries 🧭")
+    year_options = emission_temperature['YEAR'].unique()
+    selected_year = st.selectbox('Select a year', year_options)
+    selected_countries = st.multiselect('Select countries', emission_temperature['COUNTRY'].unique(), key='country A')
+    filtered_data_bar = emission_temperature[emission_temperature['COUNTRY'].isin(selected_countries) & (emission_temperature['YEAR'] == selected_year)]
+
+    display_bar_chart(filtered_data_bar, x='TOTALEMISSIONS', y='COUNTRY',
+                    title='Total Emissions for Selected Countries',
+                    xaxis_title='Country', yaxis_title='Total Emissions', width=800, height=600,color='#ff0000')
